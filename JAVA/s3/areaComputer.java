2f14874450e3e484e22223b8e7c2579a9f842c3c
Sobumbits <sobu.mbits@gmail.com>
2025-03-14 11:44:10 +0530

    Add files via upload

---
+++ JAVA/s3/areaComputer.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+public class areaComputer {
+    int a;
+    int area;
+    float x;
+    double y;
+
+    void area(int l, int b) {
+        int length = l;
+        int breadth = b;
+        a = length * breadth;
+        System.out.println("->Area: " + a);
+    }
+
+    void area(int s) {
+        int side = s;
+        area = side * side;
+        System.out.println("->Area: " + area);
+    }
+
+    void area(float b, float h) {
+        float base = b;
+        float height = h;
+        x = (base * height) / 2;
+        System.out.println("->Area: " + x);
+    }
+
+    void area(double r) {
+        double radius = r;
+        y = 3.14 * radius * radius;
+        System.out.println("->Area: " + y);
+    }
+
+    public static void main(String[] args) {
+        areaComputer obj = new areaComputer();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Area of different shapes using overloaded functions");
+
+        System.out.println("\nRECTANGLE");
+        System.out.print("->Enter the length and breadth: ");
+        int l = sc.nextInt();
+        int b = sc.nextInt();
+        obj.area(l, b);
+
+        System.out.println("\nSQUARE");
+        System.out.print("->Enter the side: ");
+        int s = sc.nextInt();
+        obj.area(s);
+
+        System.out.println("\nTRIANGLE");
+        System.out.print("->Enter the base and height: ");
+        float base = sc.nextFloat();
+        float h = sc.nextFloat();
+        obj.area(base, h);
+
+        System.out.println("\nCIRCLE");
+        System.out.print("->Enter the radius: ");
+        double r = sc.nextDouble();
+        obj.area(r);
+
+        sc.close();
+    }
+}
